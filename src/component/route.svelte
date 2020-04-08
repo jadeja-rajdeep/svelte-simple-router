42901d2fdfe326efdf2f7a8990af70087f4ff901
Rajdeepsinh Jadeja <jadeja.rajdeep@gmail.com>
2020-04-08 15:48:12 +0530

    added function to pass props to the current component from the layout component

--- src/component/route.svelte
+++ src/component/route.svelte
@@ -1,8 +1,18 @@
 <script>
 	export let currentRoute = {};
+
+	//getting rest all props other than currentRoute to pass on
+	let tempProps = $$props;
+	export let restProps = new Array();
+	for (const key in tempProps) {
+		if (key !== "currentRoute") {
+			restProps[key] = tempProps[key];
+		}
+	}
+	//getting rest all props other than currentRoute to pass on
 </script>
-{#if currentRoute.layout.viewed==false}
-	<svelte:component this={currentRoute.layout.layout} currentRoute={{ ...currentRoute, layout:{layout:currentRoute.layout.layout,viewed:true}}}/>
-{:else if currentRoute.component.viewed==false}
-	<svelte:component this={currentRoute.component.component} currentRoute={{ ...currentRoute, component:{layout:currentRoute.component.component,viewed:true}}}/>
+{#if currentRoute.layout.viewed == false}
+	<svelte:component this={currentRoute.layout.layout} {...restProps} currentRoute={{ ...currentRoute, layout:{layout:currentRoute.layout.layout,viewed:true}}}/>
+{:else if currentRoute.component.viewed == false}
+	<svelte:component this={currentRoute.component.component} {...restProps} currentRoute={{ ...currentRoute, component:{layout:currentRoute.component.component,viewed:true}}}/>
 {/if}
